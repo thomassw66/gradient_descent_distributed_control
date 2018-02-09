01e43e19b77bac69d7470cd3d1b83faa3e64a277
thomassw66 <thomassw66@gmail.com>
2018-02-09 13:10:45 -0700

    add cost function contour in background

--- EllipsoidCostFunction.m
+++ EllipsoidCostFunction.m
@@ -13,14 +13,14 @@ classdef EllipsoidCostFunction
             obj.a = a;
             obj.b = b;
          end
-         function res = calculate_cost_fn(obj, p)
-            res = (1/obj.a^2)*p(1)^2 + (1/obj.b^2)*p(2)^2 ;
+         function res = calculate_cost_fn(obj, x, y)
+            res = ((1/obj.a^2)*x.^2) + ((1/obj.b^2)*y.^2);
          end
-         function res = calculate_cost_fn_grad(obj, p)
+         function res = calculate_cost_fn_grad(obj, x, y)
             % grad = (1/a^2) 2x dx + (1/b^2) 2y dy = dz 
-             gx = (1/obj.a^2)*2*p(1);
-             gy = (1/obj.b^2)*2*p(2);
-             res = [gx, gy];
+             gx = x.*(1/obj.a^2)*2;
+             gy = y.*(1/obj.b^2)*2;
+             res = [gx, gy]
          end
      end
 
--- grad_descent.m
+++ grad_descent.m
@@ -1,36 +1,43 @@
 
+MAX_TIME = 50;
+NUM_AGENTS = 10;
 
 
-MAX_TIME = 100;
-% control loop 
+%initial_p = (rand(10,2)-0.5).*10 ; %[ [2 4]; [5 -5]; [-3 3]; ]
+Px = (rand(NUM_AGENTS,1)-0.5).*10;
+Py = (rand(NUM_AGENTS,1)-0.5).*10;
+%p = initial_p;
+%[m, n] = size(p);
 
-initial_p = (rand(10,2)-0.5).*10 ; %[ [2 4]; [5 -5]; [-3 3]; ]
-p = initial_p;
-[m, n] = size(p);
+
+cost = EllipsoidCostFunction(0.4, 2);
 
 x = linspace(-5, 5, 20);
 y = linspace(-5, 5, 20);
-X, Y = meshgrid(x,y);
-
+[X, Y] = meshgrid(x,y);
+Z = cost.calculate_cost_fn(X, Y);
 
 % move step size each time step
 % this would be analogous to something like the robots max speed
 step_size = 0.1;
 
-cost = EllipsoidCostFunction(1, 1);
-
 %Z = cost.calculate_cost_fn(XY)
 %contourf(Z);
 
+% control loop 
 for i=0:MAX_TIME 
-    for j=1:m     
-        plot(p(j,1), p(j,2), 'or', 'MarkerSize', 5, 'MarkerFaceColor', 'r')
-        axis([-5 5 -5 5]);
-        hold on
-        grad = cost.calculate_cost_fn_grad(p(j, :));
-        p(j,1) = p(j,1) - step_size * grad(1)/norm(grad);
-        p(j,2) = p(j,2) - step_size * grad(2)/norm(grad);
-    end
+    clf('reset');
+    hold on
+    contour(X, Y, Z, 20);
+    plot(Px, Py, 'or', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
+    grad = cost.calculate_cost_fn_grad(Px, Py);
+    % update positions
+    for j=1:NUM_AGENTS
+        g = grad(j, :);
+        g = g./norm(g);
+        Px(j) = Px(j) - step_size * g(1);
+        Py(j) = Py(j) - step_size * g(2);
+    end 
     hold off
     pause(0.05);
 end 
